15dc8aad02898f1454019bb0eaab7e3090cb8696
Monzir Mukhtar <75255668+helomo@users.noreply.github.com>
2022-05-07 13:37:43 +0300

    helpful methods in client

--- src/Client.java
+++ src/Client.java
@@ -59,4 +59,35 @@ public class Client {
 
       // socket.close();
    } 
+void login(OutputStream outputStream,String username, String password) throws IOException{
+       outputStream.write(("login "+username+" "+password+"\n").getBytes());
+
+   }
+void logout(OutputStream outputStream) throws IOException{
+    outputStream.write(("logout\n").getBytes());
+
+}
+void reg(OutputStream outputStream,String username, String password) throws IOException{
+    outputStream.write(("reg "+username+" "+password+"\n").getBytes());
+
+}
+void quit(OutputStream outputStream) throws IOException{
+    outputStream.write(("quit\n").getBytes());
+
+}
+void msg(OutputStream outputStream,String reciver, String theMsg) throws IOException{
+    outputStream.write(("msg "+reciver+" "+theMsg+"\n").getBytes());
+
+}
+void join(OutputStream outputStream,String groupName) throws IOException{
+    outputStream.write(("join "+groupName+"\n").getBytes());
+}
+void leave(OutputStream outputStream,String groupName) throws IOException{
+    outputStream.write(("leave "+groupName+"\n").getBytes());
+}
+void groupMsg(OutputStream outputStream,String groupName, String theMsg) throws IOException{
+    outputStream.write(("GroupMsg "+groupName+" "+theMsg+"\n").getBytes());
+
+}
+
 }
